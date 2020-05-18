d4f807b545eff7b8b21b2d0682c77bbff2472025
Mins97 <tkarnrwl7862@naver.com>
2020-05-19 00:24:06 +0900

    FirebaseHosting

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 'use strict'
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -16,7 +17,13 @@ module.exports = {
             loaders: 'babel-loader'
         }]
     },
-    plugins: [],
+    plugins: [
+        new CopyWebpackPlugin({
+          patterns: [
+            { context: './public', from: '*.*'},
+          ],
+        }),
+      ],
     devServer: {
         contentBase: './public',
         host: 'localhost',
